925bb857f33ab01e0b4276fb336ab08bde24df0c
Yichao Bu <ichaobuster@gmail.com>
2022-06-17 10:41:06 +0800

    Update dlsite.sh

--- dlsite.sh
+++ dlsite.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # でぃーえる農場
-curl -sIL -X GET "https://www.dlsite.com/books/event/dlfarm/ajax?act=draw" -H "Cookie:$1"
+curl -sL -X GET "https://www.dlsite.com/books/event/dlfarm/ajax?act=draw" -H "Cookie:$1"
 
 exit 0
