4c468c60a5f59d33bee6ef89d1b17ba42370983d
Yichao Bu <ichaobuster@gmail.com>
2023-02-26 09:36:20 +0800

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -23,6 +23,9 @@ client.send_message("@renzhecloudbot", '/checkin')
 # messages = client.get_messages('@PronembyTGBot2_bot')
 # messages[0].click(1)
 client.send_message("@PronembyTGBot2_bot", '/checkin')
+time.sleep(15)
+messages = client.get_messages('@PronembyTGBot2_bot')
+messages[0].click(1)
 
 # 标记已读
 time.sleep(3)
